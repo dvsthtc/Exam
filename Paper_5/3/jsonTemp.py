c233bc93e7dca2db7182c7d61653a134392d4019
Anna Umanets <ann4193@gmail.com>
2017-05-30 10:28:43 +0300

    added finished task 3 and new task 4

---
+++ Paper_5/3/jsonTemp.py
@@ -0,0 +1,25 @@
+import json
+import urllib
+
+serviceurl = raw_input('Enter location: ')
+if not serviceurl: serviceurl = 'http://python-data.dr-chuck.net/comments_283750.json'
+
+print 'Retrieving', serviceurl
+uh = urllib.urlopen(serviceurl)
+data = uh.read()
+
+#TODO
+#Make dictionary from elements in xml file where: key - name, value - count 
+jdata = json.loads(data)
+nlist = []
+for entry in jdata['comments']:
+        name = entry.get('name')
+        count = entry.get('count')
+        nlist.append([name,count])
+ 
+res = dict(nlist)
+
+for i in range(0,len(nlist)):
+        print nlist[i][0],nlist[i][1]
+
+print 'Dictionary: ',res
